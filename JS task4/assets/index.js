9a250c378bb112901427d6e2f2e11472ceb64746
unknown <rogachan.otaku@gmail.com>
2023-10-17 10:02:18 +0300

    updating

--- JS task4/assets/index.js
+++ JS task4/assets/index.js
@@ -1,8 +1,20 @@
-const btn = document.querySelector("button");
-const alertBox = document.querySelector(".alertBox")
-const container = document.querySelector(".container")
-btn.addEventListener("click" , (e)=> {
-alertBox.style.display = 'flex';
-btn.toggle ('container')
-    console.log(e.target);
-}) 
+// const btn = document.querySelector("button");
+// const alertBox = document.querySelector(".alertBox")
+// const container = document.querySelector(".container")
+// btn.addEventListener("click" , (e)=> {
+// alertBox.style.display = 'flex';
+// btn.classList.toggle ('container')
+//     console.log(e.target);
+// }) 
+const alertContainer = document.querySelector(".alert-container");
+const containerBody = document.querySelector(".container-body button ");
+const okBtn = document.querySelector(".ok");
+containerBody.addEventListener("click", () => {
+  alertContainer.style.display = "flex";
+});
+okBtn.addEventListener("click", () => {
+  alertContainer.style.display = "none";
+});
+alertContainer.addEventListener("click", () => {
+  alertContainer.style.display = "none";
+});
